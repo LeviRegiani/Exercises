7aea362f964a6e84b9acd5fab5ec4bee0d2ec5fa
LeviRegiani <levi.regiani@usp.br>
2022-05-12 22:17:10 -0300

    new solution to merge-two-sorted-lists problem

--- Merge Two Sorted Lists/merge-two-sorted-lists.cpp
+++ Merge Two Sorted Lists/merge-two-sorted-lists.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 
+using namespace std;
+
 // https://leetcode.com/problems/merge-two-sorted-lists/
 // You are given the heads of two sorted linked lists list1 and list2.
 // Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
@@ -26,72 +28,59 @@
 // 	-100 <= Node.val <= 100
 // 	Both list1 and list2 are sorted in non-decreasing order.
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+// Definition for singly-linked list.
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
 	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-		ListNode *result = nullptr;
 		if (list1 == NULL) {
 			return list2;
 		} else if (list2 == NULL) {
 			return list1;
 		}
-		
-		if (list1->val < list2->val) {
-			result = list1;
-		} else {
+		ListNode *result = list1;
+		if (list1->val > list2->val) {
 			result = list2;
+			list2 = list2->next;
+		} else {
+			list1 = list1->next;
 		}
 		
-		if (list1->val <= list2->val) {
-			ListNode *tmp = list1->next;
-			if (tmp->val < list2->val) {
-				list1->next = tmp;
-				list1 = tmp;
-				tmp = tmp->next;
+		ListNode *curr = result;
+		
+		while (list1 && list2) {
+			if (list1->val < list2->val) {
+				curr->next = list1;
+				list1 = list1->next;
 			} else {
-				list1->next = list2;
+				curr->next = list2;
 				list2 = list2->next;
-				
 			}
+			curr = curr->next;
 		}
 		
-		// if (list1->val <= list2->val) {
-		//     result = list1;
-		//     list1 = list1->next;
-		//     result->next = list2;
-		//     tmp = result->next;
-		// } else {
-		//     result = list2;
-		//     list2 = list2->next;
-		//     result->next = list1;
-		//     tmp = result->next;
-		// }
-		// while ((list1->next != NULL) && (list2->next != NULL)) {
-		//     if ((list1->val <= list2->val) && (list2->val < list1->next->val)) {
-		//         list1 = list1->next;
-		//         tmp->next = list2;
-		//         tmp = tmp->next;
-		//     } else {
-		//         list2 = list2->next;
-		//         tmp->next = list1;
-		//         tmp = tmp->next;
-		//     }
-		// }
-		// if (list1->val <= list2->val) {
-		//     list1->next = list2;
-		// } else {
-		//     list2->next = list1;
-		// }
-		// return result;
+		if (!list1)
+			curr->next = list2;
+		else
+			curr->next = list1;
+		
+		return result;
 	}
 };
+
+int main() {
+	Solution mySolution;
+	ListNode<ListNode> *list1 = [1, 2, 3];
+	ListNode<ListNode> *list2 = [2, 3, 5];
+	ListNode *result = mySolution.mergeTwoLists(list1, list2);
+	cout << result << "\n";
+	return 0;
+}
